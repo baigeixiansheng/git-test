a82aaa8b0b10e6eb5f63629dc859d8ccc7134fa7
baigeixiansheng <Whitegive@qq.com>
2022-07-31 15:15:00 +0800

    thiird commit

--- src/main/java/com/itheima/git/Git_Test.java
+++ src/main/java/com/itheima/git/Git_Test.java
@@ -2,7 +2,10 @@ package com.itheima.git;
 
 public class Git_Test {
     public static void main(String[] args) {
+
         System.out.println("Hello Git");
         System.out.println("Hello Baigei！");
+        System.out.println("Hello World!");
+
     }
 }
