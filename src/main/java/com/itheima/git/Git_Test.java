608a75781baf28737ca51cfd27f5495468bbf756
baigeixiansheng <Whitegive@qq.com>
2022-07-31 18:44:01 +0800

    master commit2

--- src/main/java/com/itheima/git/Git_Test.java
+++ src/main/java/com/itheima/git/Git_Test.java
@@ -6,6 +6,7 @@ public class Git_Test {
         System.out.println("Hello Git");
         System.out.println("Hello Baigei！");
         System.out.println("Hello World4!");
+        System.out.println("master test!");
 
     }
 }
